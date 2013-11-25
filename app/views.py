f5be6e03f7e10e29556a6ce86ebc607a44adbcd6
swgillespie <sean.william.g@gmail.com>
2013-11-25 13:44:10 -0500

    Complete pickup page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, g, redirect, request
-from app.decorators import requires_login
+from flask import Blueprint, render_template, g, redirect, request, url_for
+# suppress pyflakes warning
+# from app.decorators import requires_login
 from app.forms import LoginForm, RegistrationForm
+
 
 mod = Blueprint('tools', __name__, url_prefix='/')
 
@@ -27,3 +29,45 @@ def register():
         )
         return redirect('/')
     return render_template('register.html', form=form)
+
+@mod.route('pickup/', methods=['GET', 'POST'])
+def pickup():
+    if request.method == 'GET':
+        return render_template('pickup.html')
+    elif request.method == 'POST':
+        if 'pickup' in request.form:
+            pickup_day = request.form['pickup']
+            ## DO DB QUERY HERE 
+            rows = [{
+                'client_id': 42,
+                'lastname': 'gillespie',
+                'firstname': 'sean',
+                'family_size': 8,
+                'street': '123 Easy Street',
+                'city': 'Compton',
+                'state': 'CA',
+                'zip': 12345,
+                'apartment': 9999,
+                'phone': '123-456-7890',
+                'pickup_day': 9
+            },
+            {
+                'client_id': 42,
+                'lastname': 'gillespie',
+                'firstname': 'sean',
+                'family_size': 8,
+                'street': '123 Easy Street',
+                'city': 'Compton',
+                'state': 'CA',
+                'zip': 12345,
+                'apartment': 9999,
+                'phone': '123-456-7890',
+                'pickup_day': 9
+            }
+
+            ]
+            ## END DB TRANSACTION
+            return render_template('pickup.html', rows=rows)
+    # other methods are not supported
+    return render_template('pickup.html')
+    
