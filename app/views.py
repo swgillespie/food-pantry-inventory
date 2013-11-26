2edac738a170444698cc4f1eb5a6dacd89016f80
swgillespie <sean.william.g@gmail.com>
2013-11-25 22:15:34 -0500

    DB state functions. Will document more later

--- app/views.py
+++ app/views.py
@@ -3,9 +3,30 @@ from flask import Blueprint, render_template, g, redirect, request, url_for, fla
 # from app.decorators import requires_login
 from app.forms import LoginForm, RegistrationForm, DropoffForm, NewProductForm
 import base64
+from app.db import DBInterface
 
 
 mod = Blueprint('tools', __name__, url_prefix='/')
+
+def connect_db():
+    conn = DBInterface(app.config['DATABASE'])
+    return conn
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.execute_script(f.read())
+
+def get_db():
+    if not hasattr(g, sqlite_db):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_environment
+def close_db(error):
+    if hasattr(g, sqlite_db):
+        g.sqlite_db.close()
 
 @mod.route('/', methods=['GET', 'POST'])
 def login():
