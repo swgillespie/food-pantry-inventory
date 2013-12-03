f86593b306c8e01660626f74999b520dc3901869
Chris <chris@bonjwa.(none)>
2013-12-03 15:24:01 -0500

    useless commit message

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, TextField, PasswordField, BooleanField, validators
+from wtforms import Form, TextField, PasswordField, BooleanField, IntegerField, validators
 
 class LoginForm(Form):
     username = TextField("Username", [
@@ -33,3 +33,55 @@ class RegistrationForm(Form):
     ])
     confirm_pass = PasswordField("Password Again")
     is_director = BooleanField("Are you a director?")
+
+class ClientForm:
+    pick_up_day = IntegerField("Pick-up Day", [
+	validators.Length(min=1, max=2),
+	validators.InputRequired(message="This field is required.")
+	validators.NumberRange(min=1, max=28, message="Date must be between 1 and 28.")
+    ])
+    first_name = TextField("First Name", [
+	validators.Length(min=2, max=40),
+	validators.InputRequired(message="This field is required.")
+    ])
+    last_name = TextField("Last Name", [
+	validators.Length(min=2, max=40),
+	validators.InputRequired(message="This field is required.")
+    ])
+    gender = TextField("Gender", [
+	validators.Length(min=2, max=40),
+	validators.InputRequired(message="This field is required.")
+    ])
+    birthdate = TextField("Birth-date: (mm/dd/YYYY)", [
+	validators.Length(min=10, max=10),
+	validators.InputRequired(message="This field is required.")
+    ])
+    street = TextField("Street Name", [
+	validators.Length(min=2, max=40),
+	validators.InputRequired(message="This field is required.")
+    ])
+    apartment_num = IntegerField("Apartment #", [
+	validators.Length(min=1, max=10),
+	validators.InputRequired(message="This field is required.")
+    ])
+    city = TextField("City", [
+	validators.Length(min=2, max=40),
+	validators.InputRequired(message="This field is required.")
+    ])
+    state = TextField("State", [
+	validators.Length(min=2, max=40),
+	validators.InputRequired(message="This field is required.")
+    ])
+    zipcode = IntegerField("Zipcode", [
+	validators.Length(min=5, max=5),
+	validators.InputRequired(message="This field is required.")
+    ])
+    phone = IntegerField("Phone Number: XXX-XXXX", [
+	validators.Length(min=8, max=8),
+	validators.InputRequired(message="This field is required.")
+    ])
+    start_date = IntegerField("Start Date: (mm/dd/YYYY)", [
+	validators.Length(min=10, max=10),
+	validators.InputRequired(message="This field is required.")
+    ])
+    delivery = BooleanField("Are you picking up the bag?")
